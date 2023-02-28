f1253b256ad5e6ed6fbeb8db0b7ce40c591e1514
ParthSuthar1012 <104064849+ParthSuthar1012@users.noreply.github.com>
2023-02-28 18:15:22 +0530

    Add files via upload

---
+++ Hospital-Management-System-main/src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    projectId: 'management-system-277cf',
+    appId: '1:1075474283757:web:af16b311f53c6516a560e4',
+    storageBucket: 'management-system-277cf.appspot.com',
+    apiKey: 'AIzaSyB3HUADFjvZUggwbfB0CkJSgFNk75yLMOk',
+    authDomain: 'management-system-277cf.firebaseapp.com',
+    messagingSenderId: '1075474283757',
+  }
+};
+
